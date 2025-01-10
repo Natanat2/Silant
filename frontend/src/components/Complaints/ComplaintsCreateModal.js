2e9e6279182c803e7fed0848283e0505d38b276b
Natanat2 <natanat2@mail.ru>
2025-01-10 23:24:37 +0500

    add complaints create

--- frontend/src/components/Complaints/ComplaintsCreateModal.js
+++ frontend/src/components/Complaints/ComplaintsCreateModal.js
@@ -1,13 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Modal, Button, Form, Toast, Spinner } from "react-bootstrap";
+import { Modal, Button, Form, Toast } from "react-bootstrap";
 import axios from "axios";
 
-const ComplaintsCreateModal = ({
-  show,
-  onClose,
-  onSave,
-  machineId, // ID машины
-}) => {
+const ComplaintsCreateModal = ({ show, onClose, onSave, machineId }) => {
   const [localFormData, setLocalFormData] = useState({
     failure_node: "",
     method_of_recovery: "",
@@ -23,11 +18,8 @@ const ComplaintsCreateModal = ({
     recoveryMethods: [],
     serviceCompanies: [],
   });
-  const [loading, setLoading] = useState(false);
   const [showSuccessToast, setShowSuccessToast] = useState(false);
-  const [error, setError] = useState(null);
 
-  // Загрузка зависимостей для формы
   useEffect(() => {
     const fetchDependencies = async () => {
       try {
@@ -46,7 +38,6 @@ const ComplaintsCreateModal = ({
         });
       } catch (err) {
         console.error("Ошибка при загрузке зависимостей:", err);
-        setError("Не удалось загрузить данные зависимостей.");
       }
     };
 
@@ -63,18 +54,9 @@ const ComplaintsCreateModal = ({
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true);
-    setError(null);
 
     try {
       const token = localStorage.getItem("access_token");
-      if (!token) {
-        setError("Токен авторизации отсутствует");
-        setLoading(false);
-        return;
-      }
-
-      // Сопоставление данных для service_company_maintenance
       const selectedServiceCompany = dependencies.serviceCompanies.find(
         (company) =>
           company.id.toString() === localFormData.service_company_maintenance
@@ -82,8 +64,8 @@ const ComplaintsCreateModal = ({
 
       const requestData = {
         ...localFormData,
-        machine: machineId, // ID машины из пропсов
-        service_company_maintenance: selectedServiceCompany.id, // Сопоставленный ID
+        machine: machineId,
+        service_company_maintenance: selectedServiceCompany.id,
       };
 
       console.log("Отправляемые данные:", requestData);
@@ -103,11 +85,6 @@ const ComplaintsCreateModal = ({
       }, 500);
     } catch (error) {
       console.error("Ошибка при создании жалобы:", error);
-      setError(
-        "Не удалось создать рекламацию. Проверьте введённые данные и попробуйте снова."
-      );
-    } finally {
-      setLoading(false);
     }
   };
 
@@ -135,120 +112,111 @@ const ComplaintsCreateModal = ({
           <Modal.Title>Создать рекламацию</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {loading ? (
-            <div className="text-center">
-              <Spinner animation="border" />
-              <p>Загрузка...</p>
-            </div>
-          ) : (
-            <Form onSubmit={handleSubmit}>
-              <div className="row">
-                <div className="col-md-6">
-                  <Form.Group className="mb-3">
-                    <Form.Label>Узел отказа</Form.Label>
-                    <Form.Select
-                      name="failure_node"
-                      value={localFormData.failure_node}
-                      onChange={handleChange}
-                      required
-                    >
-                      <option value="">Выберите узел отказа...</option>
-                      {dependencies.failureNodes.map((node) => (
-                        <option key={node.id} value={node.id}>
-                          {node.name_of_node}
-                        </option>
-                      ))}
-                    </Form.Select>
-                  </Form.Group>
-                  <Form.Group className="mb-3">
-                    <Form.Label>Способ восстановления</Form.Label>
-                    <Form.Select
-                      name="method_of_recovery"
-                      value={localFormData.method_of_recovery}
-                      onChange={handleChange}
-                      required
-                    >
-                      <option value="">
-                        Выберите способ восстановления...
+          <Form onSubmit={handleSubmit}>
+            <div className="row">
+              <div className="col-md-6">
+                <Form.Group className="mb-3">
+                  <Form.Label>Узел отказа</Form.Label>
+                  <Form.Select
+                    name="failure_node"
+                    value={localFormData.failure_node}
+                    onChange={handleChange}
+                    required
+                  >
+                    <option value="">Выберите узел отказа...</option>
+                    {dependencies.failureNodes.map((node) => (
+                      <option key={node.id} value={node.id}>
+                        {node.name_of_node}
                       </option>
-                      {dependencies.recoveryMethods.map((method) => (
-                        <option key={method.id} value={method.id}>
-                          {method.name_of_method}
-                        </option>
-                      ))}
-                    </Form.Select>
-                  </Form.Group>
-                  <Form.Group className="mb-3">
-                    <Form.Label>Дата отказа</Form.Label>
-                    <Form.Control
-                      type="date"
-                      name="date_of_refusal"
-                      value={localFormData.date_of_refusal}
-                      onChange={handleChange}
-                      required
-                    />
-                  </Form.Group>
-                  <Form.Group className="mb-3">
-                    <Form.Label>Наработка, м/час</Form.Label>
-                    <Form.Control
-                      type="number"
-                      name="operating_time_refusal"
-                      value={localFormData.operating_time_refusal}
-                      onChange={handleChange}
-                      required
-                    />
-                  </Form.Group>
-                </div>
-                <div className="col-md-6">
-                  <Form.Group className="mb-3">
-                    <Form.Label>Описание отказа</Form.Label>
-                    <Form.Control
-                      as="textarea"
-                      name="description_of_refusal"
-                      value={localFormData.description_of_refusal}
-                      onChange={handleChange}
-                      required
-                    />
-                  </Form.Group>
-                  <Form.Group className="mb-3">
-                    <Form.Label>Используемые запчасти</Form.Label>
-                    <Form.Control
-                      type="text"
-                      name="spare_parts_used"
-                      value={localFormData.spare_parts_used}
-                      onChange={handleChange}
-                    />
-                  </Form.Group>
-                  <Form.Group className="mb-3">
-                    <Form.Label>Дата восстановления</Form.Label>
-                    <Form.Control
-                      type="date"
-                      name="restoration_date"
-                      value={localFormData.restoration_date}
-                      onChange={handleChange}
-                      required
-                    />
-                  </Form.Group>
-                  <Form.Group className="mb-3">
-                    <Form.Label>Сервисная компания</Form.Label>
-                    <Form.Select
-                      name="service_company_maintenance"
-                      value={localFormData.service_company_maintenance}
-                      onChange={handleChange}
-                      required
-                    >
-                      <option value="">Выберите сервисную компанию...</option>
-                      {dependencies.serviceCompanies.map((company) => (
-                        <option key={company.id} value={company.id}>
-                          {company.user.first_name}
-                        </option>
-                      ))}
-                    </Form.Select>
-                  </Form.Group>
-                </div>
+                    ))}
+                  </Form.Select>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Способ восстановления</Form.Label>
+                  <Form.Select
+                    name="method_of_recovery"
+                    value={localFormData.method_of_recovery}
+                    onChange={handleChange}
+                    required
+                  >
+                    <option value="">Выберите способ восстановления...</option>
+                    {dependencies.recoveryMethods.map((method) => (
+                      <option key={method.id} value={method.id}>
+                        {method.name_of_method}
+                      </option>
+                    ))}
+                  </Form.Select>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Дата отказа</Form.Label>
+                  <Form.Control
+                    type="date"
+                    name="date_of_refusal"
+                    value={localFormData.date_of_refusal}
+                    onChange={handleChange}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Наработка, м/час</Form.Label>
+                  <Form.Control
+                    type="number"
+                    name="operating_time_refusal"
+                    value={localFormData.operating_time_refusal}
+                    onChange={handleChange}
+                    required
+                  />
+                </Form.Group>
               </div>
-            </Form>
-          )}
+              <div className="col-md-6">
+                <Form.Group className="mb-3">
+                  <Form.Label>Описание отказа</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    name="description_of_refusal"
+                    value={localFormData.description_of_refusal}
+                    onChange={handleChange}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Используемые запчасти</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="spare_parts_used"
+                    value={localFormData.spare_parts_used}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Дата восстановления</Form.Label>
+                  <Form.Control
+                    type="date"
+                    name="restoration_date"
+                    value={localFormData.restoration_date}
+                    onChange={handleChange}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Сервисная компания</Form.Label>
+                  <Form.Select
+                    name="service_company_maintenance"
+                    value={localFormData.service_company_maintenance}
+                    onChange={handleChange}
+                    required
+                  >
+                    <option value="">Выберите сервисную компанию...</option>
+                    {dependencies.serviceCompanies.map((company) => (
+                      <option key={company.id} value={company.id}>
+                        {company.user.first_name}
+                      </option>
+                    ))}
+                  </Form.Select>
+                </Form.Group>
+              </div>
+            </div>
+          </Form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="primary" onClick={handleSubmit}>
